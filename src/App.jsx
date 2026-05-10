c6dd619e3fb73ec80dd2959594b0e45fd5d54cb3
Abu Bakor Siddik (Shuvo) <141806136+Abs-shuvo@users.noreply.github.com>
2026-05-10 16:06:57 +0600

    Added banner succeffully with responsiveness, install lucide-react & react-spinner for use purpose

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 
 import './App.css'
+import Banner from './components/Banner/Banner'
 import NavBar from './components/NavBar/NavBar'
 
 function App() {
   return (
     <>
       <NavBar></NavBar>
+      <Banner></Banner>
     </>
   )
 }
---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,34 @@
+import { Play } from 'lucide-react';
+import React from 'react';
+import { BounceLoader } from 'react-spinners';
+const Banner = () => {
+    return (
+        <div className="hero bg-base-200 container mx-auto py-20 px-30">
+            <div className="hero-content flex-col lg:flex-row-reverse xl:gap-50">
+                <img
+                src="https://i.ibb.co.com/V0PVnz2K/banner.png"
+                className="max-w-sm rounded-lg"
+                />
+                <div>
+                    <div>
+                        <div className="badge badge-soft badge-primary mb-4"> <BounceLoader size={12} color="#7e00fc"></BounceLoader> New: AI-Powered Tools Available</div>
+
+                    </div>
+                <h1 className='text-5xl font-extrabold! mb-1'>Supercharge Your</h1>
+                <h1 className='text-5xl font-extrabold!'>Digital Workflow</h1>
+                <p className="py-6 text-[#627382] font-regular ">
+                    Access premium AI tools, design assets, templates, and productivity <br />
+                    software—all in one place. Start creating faster today. <br />
+                    Explore Products
+                </p>
+                <div>
+                    <button className="btn btn-primary rounded-full bg-linear-to-r from-[#4f39f6] to-[#9514fa] mr-4">Explore Product</button>
+                    <button className="btn btn-neutral btn-outline rounded-full border-[#9514fa]  "> <Play color='#9434f4'></Play> <span className='text-transparent bg-clip-text bg-linear-to-r from-[#4f39f6] to-[#9514fa] '>Outline</span></button>
+                </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
